97b99801c2f02c1e05050886c0b95afe3777e6bc
James Seward <james@jamesoff.net>
2018-01-31 21:36:11 +0000

    Post to SQS after link is ready

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -7,28 +7,36 @@ def lambda_handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = urllib.unquote(record['s3']['object']['key'])
+        key_filename = key.split('/')[1]
         print('Downloading template')
         template_body_response = s3.get_object(
             Bucket=bucket,
             Key='template/template.html'
         )
         template_body = template_body_response['Body'].read()
-        target_object = 'v/{}'.format(record['s3']['object']['eTag'][0:8])
+        target_filename = record['s3']['object']['eTag'][0:8]
+        target_object = 'v/{}'.format(target_filename)
         rendered_template = bytes(pystache.render(template_body, {
             'url': key,
-            'title': urllib.unquote_plus(key.split('/')[1])
+            'title': urllib.unquote_plus(key_filename)
         }))
         print('Writing rendered template to {}'.format(target_object))
         s3.put_object(
             Bucket=bucket,
             Key=target_object,
             Body=rendered_template,
-            ContentType='text/html'
+            ContentType='text/html',
+            StorageClass='STANDARD_IA'
+        )
+        sqs.send_message(
+            QueueUrl='https://sqs.eu-west-1.amazonaws.com/108685319098/cloud-output',
+            MessageBody='{}={}'.format(key_filename, target_object)
         )
         print('Done')
 
 
 s3 = boto3.client('s3')
+sqs = boto3.client('sqs')
 
 if __name__ == '__main__':
     lambda_handler({'Records': [
