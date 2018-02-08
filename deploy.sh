781a93b1418f5c56116b9772ebcedfc84e7dcdf9
James Seward <james@jamesoff.net>
2018-02-08 17:59:56 +0000

    Take bucket from env; do pip install before zip

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,19 @@
 
 set -e
 
+bucket=${CLOUD_BUCKET?"Missing CLOUD_BUCKET in environment"}
+
 echo Uploading template and assets
-aws s3 sync s3-files/template/ s3://cloud.jamesoff.net/template --delete --exact-timestamps
-aws s3 sync s3-files/inc/ s3://cloud.jamesoff.net/inc --delete --exact-timestamps
-aws s3 cp s3-files/index.html s3://cloud.jamesoff.net/index.html
+aws s3 sync s3-files/template/ s3://${bucket}/template --delete --exact-timestamps
+aws s3 sync s3-files/inc/ s3://${bucket}/inc --delete --exact-timestamps
+aws s3 cp s3-files/index.html s3://${bucket}/index.html
 
 if [[ $1 == "lambda" ]]; then
 	echo Preparing lambda
 	[ -f lambda.zip ] && rm lambda.zip
 	(
 	cd lambda
+	pip install -r requirements.txt -t .
 	zip -q -r ../lambda.zip ./*
 	)
 
