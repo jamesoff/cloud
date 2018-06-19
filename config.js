92347bd8e42f14d49fb1008a5b8b952814badadb
James Seward <james@jamesoff.net>
2018-06-19 22:39:52 +0100

    WIP working upload with params file

---
+++ config.js
@@ -0,0 +1,9 @@
+var cloudConfig = {
+	QueueURL: 'https://sqs.eu-west-1.amazonaws.com/108685319098/cloud2-OutputQueue-GUYCXJFZ83C2',
+	ApplicationID: '407g0rte8cj1m9b300ebtf25ba',
+	IdentityPool: 'eu-west-1:dbb0d32a-7097-44be-b111-1b8b110c4c08',
+	LoginPool: 'cognito-idp.eu-west-1.amazonaws.com/eu-west-1_jVgMpU5vj',
+	Region: 'eu-west-1',
+	Bucket: 'cloud2.jamesoff.net',
+	AuthHost: 'cloud-test.auth.eu-west-1.amazoncognito.com'
+}
